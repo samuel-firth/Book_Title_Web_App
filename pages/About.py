5fadfa508b8b0984247672f02ea67b73e2999d1f
samfirth <samfirth1994@gmail.com>
2023-06-08 15:59:38 +0100

    Expansions for each title to allow greater custom data - aesthetic only ATM

--- pages/About.py
+++ pages/About.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import functions as fnc
 
 st.title("About")
 st.write("Hello, this is all about this app")
@@ -15,3 +16,20 @@ st.write("<u>Ideas for Improvements:</u><br>"
          "7. Add start and end dates of reading<br><br>"
          "This should allow you to compile book lists of 'to-read' and 'read'",
          unsafe_allow_html=True)
+
+titles = fnc.get_titles()
+
+for title in titles:
+    with st.expander(title):
+        st.radio("Progress", options=["In progress", "Completed"], key=f"progress{title}", horizontal=True)
+        st.text_input(label="Start Date", key=f"start{title}"),
+        st.text_input(label="End Date", key=f"end{title}")
+        st.radio("Rating", options=[1, 2, 3, 4, 5], key=f"rating{title}", horizontal=True)
+        st.text_input(label="", placeholder="Genre", key=f"genre{title}")
+        st.text_input(label="", placeholder="Author", key=f"author{title}")
+        st.slider("Length", min_value=0, max_value=1000, key=f"length{title}")
+        st.text_area(label="", placeholder="Book Summary", key=f"summary{title}")
+
+
+
+

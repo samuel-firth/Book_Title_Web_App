cafc7abd14fd29e2a9e517e464b0b0542ea5fdab
samfirth <samfirth1994@gmail.com>
2023-06-08 12:02:34 +0100

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = 'titles.txt'
+
+def get_titles(filepath=FILEPATH):
+    """ Read a text file to return the list of Titles """
+    with open(filepath, 'r') as file_local:
+        titles_local = file_local.readlines()
+    return titles_local
+
+
+def write_titles(title_arg, filepath=FILEPATH):
+    """ Write over text file with updated list of Titles """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(title_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_titles())
+
---
+++ web.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import functions as fnc
+
+titles = fnc.get_titles()
+
+
+def add_title():
+    title_local = st.session_state["new_title"] + "\n"
+    titles.append(title_local)
+    fnc.write_titles(titles)
+    st.session_state["new_title"] = ""
+
+
+st.title("Book Title App")
+st.subheader("This app lets you track what books you have read")
+st.write("Lets get reading!")
+st.write("Use the checkboxes to remove titles")
+
+for index, title in enumerate(titles):
+    checkbox = st.checkbox(title, key=title,)
+    if checkbox:
+        titles.pop(index)
+        fnc.write_titles(titles)
+        del st.session_state[title]
+        st.experimental_rerun()
+
+
+st.text_input(label="",
+              placeholder="Enter a title",
+              on_change=add_title,
+              key="new_title")
+
